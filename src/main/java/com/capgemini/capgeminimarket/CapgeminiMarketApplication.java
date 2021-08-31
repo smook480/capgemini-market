c4ab3ffba635f37b94341a8cc3cf50e861ee2020
smook480 <rovadores.de.cadaveres@gmail.com>
2021-08-31 12:32:51 -0500

    Se pusieron los archivos base

---
+++ src/main/java/com/capgemini/capgeminimarket/CapgeminiMarketApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.capgeminimarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapgeminiMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapgeminiMarketApplication.class, args);
+	}
+
+}
